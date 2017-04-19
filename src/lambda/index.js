fa9402751f0762ab7f0933c159505d5f2aba010b
Michael Smith <Michael.Smith@arcadiagroup.co.uk>
2017-04-19 20:52:49 +0100

    Adds mock storage system

--- src/lambda/index.js
+++ src/lambda/index.js
@@ -2,18 +2,26 @@ const shortid = require('shortid');
 
 exports.handler = function(event, context, callback) {
   const response = {}
-  if(!event.id) {
-    response.id = shortid.generate()
+
+  //id
+  if(event.id && context.storage.has(event.id)) {
+    Object.assign(response, context.storage.get(event.id))
   } else {
-    response.id = event.id
+    response.id = shortid.generate()
   }
+
+  //group
   response.group = []
   if(event.group) {
     response.group = response.group.concat(event.group.map((item) => {
         return { name: item, id: shortid.generate() }
     }))
   }
+
+  //list
+
   // console.log(response)
+  context.storage.set(response.id, response)
   callback(null, response)
   //See what type of event
 
--- src/lambda/index.spec.js
+++ src/lambda/index.spec.js
@@ -1,46 +1,56 @@
 const handler = require("./index").handler
 
+const mockStorage = new Map()
+const context = { storage: mockStorage }
+
 describe("handler", () => {
+  beforeEach(() => {
+    mockStorage.clear()
+  })
+
   describe("id", () => {
     it("should return a new id if the event doesn't have one", () => {
-      handler({ }, null, (err, state) => {
+      handler({ }, context, (err, state) => {
           expect(state.id).toBeDefined()
       })
     })
     it("should return a unique id", () => {
       const ids = []
-      handler({ }, null, (err, state) => {
+      handler({ }, context, (err, state) => {
         ids.push(state.id)
       })
-      handler({ }, null, (err, state) => {
+      handler({ }, context, (err, state) => {
         ids.push(state.id)
       })
       expect(ids[0]).not.toBe(ids[1])
     })
     it("should return the same id if the event already has one", () => {
-      const id = "ABCDE"
-      handler({ id }, null, (err, state) => {
+      let id = ''
+      handler({ }, context, (err, state) => {
+          id = state.id
+      })
+      handler({ id }, context, (err, state) => {
           expect(state.id).toBe(id)
       })
     })
   })
   describe("group", () => {
     it("should return a group array if the event doesn't have one", () => {
-      handler({ }, null, (err, state) => {
+      handler({ }, context, (err, state) => {
           expect(state.group).toEqual([])
       })
     })
     it("should add any additional members requested to the group", () => {
-      handler({ group: [ "Jim" ] }, null, (err, state) => {
+      handler({ group: [ "Jim" ] }, context, (err, state) => {
           expect(state.group[0].name).toBe("Jim")
       })
     })
     it("should add any additional members with a unique id", () => {
       const ids = []
-      handler({ group: [ "Jim" ] }, null, (err, state) => {
+      handler({ group: [ "Jim" ] }, context, (err, state) => {
         ids.push(state.group[0].id)
       })
-      handler({ group: [ "Jim" ] }, null, (err, state) => {
+      handler({ group: [ "Jim" ] }, context, (err, state) => {
         ids.push(state.group[0].id)
       })
       expect(ids[0]).not.toBe(ids[1])
